01c045e3ddb5aa055dcc94439cfec9f5c136f333
Leanne Keenan <l_keenan@outlook.com>
2023-03-17 14:12:41 -0700

    Added "Basic JavaScript" 11 - 20

---
+++ Basic JavaScript 11 - 20.js
@@ -0,0 +1,52 @@
+/*
+Note:
+the variables begin with a "q#_" to associate a commonly used variable (such as 'a') to the appropriate step in the course
+*/
+
+//11. Change the 0 so that sum will equal 20.
+//const sum = 10 + 0;
+const sum  = 10 + 10;
+//12. Change the 0 so the difference is 12.
+//const difference = 45 - 0;
+const difference = 45 - 33;
+//13. Change the 0 so that product will equal 80.
+//const product = 8 * 0;
+const q13_product = 8 * 10;
+//14. Change the 0 so that the quotient is equal to 2.
+//const quotient = 66 / 0;
+const quotient = 66 / 33;
+//15. Change the code to use the ++ operator on myVar.
+/*
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
+ */
+let q15_myVar = 87;
+q15_myVar++;
+//16. Change the code to use the -- operator on myVar.
+/*
+let myVar = 11;
+
+// Only change code below this line
+myVar = myVar - 1;
+*/
+let q16_myVar = 11;
+q16myVar--;
+//17. Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+//const ourDecimal = 5.7;
+
+// Only change code below this line
+
+const ourDecimal = 5.7;
+const myDecimal = 2.3;
+
+//18. Change the 0.0 so that product will equal 5.0.
+//const product = 2.0 * 0.0;
+const q18_product = 2.0 * 2.5;
+//19. Change the 0.0 so that quotient will equal to 2.2.
+//const quotient = 0.0 / 2.0; // Change this line
+const q_19quotient = 4.4 / 2.0; // Change this line
+//20. Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+//const remainder = 0;
+const remainder = 11 % 3;
