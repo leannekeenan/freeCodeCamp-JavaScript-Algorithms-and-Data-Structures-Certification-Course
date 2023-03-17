60ae58692bf48d07ffebf14613f51bb1007a4a9d
Leanne Keenan <l_keenan@outlook.com>
2023-03-16 17:09:36 -0700

    Added "Basic JavaScript" 1 - 10

---
+++ Basic JavaScript 1-10.js
@@ -0,0 +1,74 @@
+/*
+Note:
+the variables begin with a "q#_" to associate a commonly used variable (such as 'a') to the appropriate step in the course
+*/
+
+//1. illustrate the two types of comments in JS
+//short comment format
+/*long comment format*/
+
+//2. declare a variable named myName using the var keyword
+var myName;
+
+//3. Assign the value 7 to variable a.
+var q3_a;
+a = 7;
+
+//4. Assign the contents of a to variable b.
+var b;
+b = q3_a;
+
+
+//5. Define a variable a with var and initialize it to a value of 9.
+var q4_a = 9;
+
+
+//6. Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+var myFirstName = "Leanne";
+var myLastName = "Keenan";
+
+//7. Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+var a;
+var b;
+var c;
+// Only change code above this line
+a = 5;
+b = 10;
+c = "I am a"
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//8. Modify the existing declarations and assignments so their names use camelCase. Do not create any new variables.
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//9. Update the code so it only uses the let keyword.
+/*
+var catName = "Oliver";
+var catSound = "Meow!";
+*/
+let catName = "Oliver";
+let catSound = "Meow!"
+
+//10. Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+/*
+var fCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact); // Change this line
+*/
+const FCC = "freeCodeCamp"; // Change this line
+
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
